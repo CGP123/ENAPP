3a93251e8d6a78d4c3842346f529a83df2c08c00
peggy <peggysturm2004@gmail.com>
2023-11-10 20:50:32 +0800

    добавил кнопку

--- app/src/main/java/com/example/exampledb/MainActivity.java
+++ app/src/main/java/com/example/exampledb/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.exampledb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.exampledb.dataBase.DBManager;
 import com.example.exampledb.dataBase.MeteringDeviceTableManager;
@@ -12,14 +15,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton IB_fragmentSwitch;
+    private MainScreenFragment mainScreenFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IB_fragmentSwitch = findViewById(R.id.IB_fragmentSwitch);
+
+        IB_fragmentSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setMainScreenFragment(mainScreenFragment);
+            }
+        });
     }
 
     public void showDeviceCard(NewCardFragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit();
+    }
+
+    private void setMainScreenFragment(MainScreenFragment fragment) {
+        MainScreenFragment mainScreenFragment = new MainScreenFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragmentContainerView, mainScreenFragment).commit();
     }
 }
