09c4fc8d767b55e5cbd983958a9508915c981d30
peggy <peggysturm2004@gmail.com>
2023-11-23 18:50:59 +0800

    добавлены примеры

--- app/src/main/java/com/example/exampledb/MainActivity.java
+++ app/src/main/java/com/example/exampledb/MainActivity.java
@@ -18,13 +18,29 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton IB_fragmentSwitch;
     private MainScreenFragment mainScreenFragment;
+    private MeteringDeviceTableManager meteringDeviceTableManager;
+    private DBManager dbManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dbManager = new DBManager(this);
+        meteringDeviceTableManager = new MeteringDeviceTableManager(this);
+
         IB_fragmentSwitch = findViewById(R.id.IB_fragmentSwitch);
 
+        dbManager.openDB();
+        List<MeteringDevice> list = new ArrayList<>();
+        MeteringDevice meteringDevice1 = new MeteringDevice("АКУД15794235","Иванов И. И.","Опломбирован","21.11.2023");
+        MeteringDevice meteringDevice2 = new MeteringDevice("ИСИБ74521398","Дмитриев В.П","Опломбирован","17.03.2004");
+        MeteringDevice meteringDevice3 = new MeteringDevice("АСУБ45127894","Пупкин В. И.","Не опломбирован","14.08.2011");
+
+        list.add(meteringDevice1);
+        list.add(meteringDevice2);
+        list.add(meteringDevice3);
+
+        //meteringDeviceTableManager.addNewMeteringDevice(list);
         IB_fragmentSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
